9a77167e0dfa0cc4b9cbe4eedc2c8ab897e798d1
Evandro Harrison Hoffmann <ehh85@yahoo.com.br>
2016-09-27 15:46:03 +0800

    Adds fix for formatting Data into NSAttributedString in Swift 3.

--- AwesomeData/Classes/Formatting/Extensions.swift
+++ AwesomeData/Classes/Formatting/Extensions.swift
@@ -13,7 +13,7 @@ extension Data {
         do {
             return try NSAttributedString(data: self, options:[
                 NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType,
-                NSCharacterEncodingDocumentAttribute: String.Encoding.utf8
+                NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue
                 ], documentAttributes: nil)
         } catch let error as NSError {
             print(error.localizedDescription)
