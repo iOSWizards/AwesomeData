2be74bacae441911371adce9940e3734754c2590
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-31 21:13:05 +0800

    Fixed Xcode warning

--- AwesomeData/Classes/Placeholder/AwesomeBackgroundPlaceholder.swift
+++ AwesomeData/Classes/Placeholder/AwesomeBackgroundPlaceholder.swift
@@ -61,7 +61,7 @@ extension UIView {
                     ])
             } else if constraint.secondItem as? NSObject == self {
                 self.superview?.addConstraints([
-                    NSLayoutConstraint(item: constraint.firstItem,
+                    NSLayoutConstraint(item: constraint.firstItem ?? 0,
                                        attribute: constraint.firstAttribute,
                                        relatedBy: constraint.relation,
                                        toItem: toView,
